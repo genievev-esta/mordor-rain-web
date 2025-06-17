8d6d7863bbaa6dad28759a5b7e5be62500b2104f
Genievev Esta <genievev.esta08@gmail.com>
2025-06-17 11:44:49 +0000

    Update main.py with async loop for pygbag

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import pygame
-import sys
-import os
-
-os.environ["SDL_AUDIODRIVER"] = "dummy" #TEMP: for audio driver warn
-
-SCREEN_WIDTH=720
-SCREEN_HEIGHT=1280
+import asyncio, pygame
 
 pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
 
-#create surface
-screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
-screen.fill("#1a1a1a")
+async def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.draw.circle(screen, (255, 0, 0), (400, 300), 30)
+        pygame.display.flip()
+        clock.tick(60)
+        await asyncio.sleep(0)
 
-pygame.quit()
+asyncio.run(main())
